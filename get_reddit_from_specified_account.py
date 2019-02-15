290028280af782ed07e8d7465b18a2dc4d1e7088
Nicola Zotto <nzotto18@student.oulu.fi>
2019-02-15 14:43:40 +0100

    updated data collection scripts
    Changed the way to collect data from twitter and reddit so that they can be merged better with the website.

--- get_reddit_from_specified_account.py
+++ get_reddit_from_specified_account.py
@@ -6,15 +6,15 @@
 import praw
 from praw.models import MoreComments
 
-
-## authentification data. We need to create an app on this page:
-## https://www.reddit.com/prefs/apps Should we create a dedicated reddit accoutn?
-def create_reddit_oject():
+## We need to create an app on this page:
+## https://www.reddit.com/prefs/apps
+## Should we create a dedicated reddit account?
+def identification_reddit():
     """
-    authentification data. We need to create an app on this page:
-    https://www.reddit.com/prefs/apps Should we create a dedicated reddit accoutn?
+    create instance for the data collection API
     :return: an instace of reddit that can be used to retrieve information on the user
     """
+    ## details for reddit application
     app_id = 'lAFoUYMH9NC96w'
     app_secret = 'I_ekhjz-VCYkjOxJ4NhutP6yW2Q'
     app_name = 'DepSenTest'
@@ -24,9 +24,10 @@ def create_reddit_oject():
                          user_agent= app_name)
     return res
 
-def get_comments_from_user(reddit,username):
+def get_reddit_data(reddit, username):
     """
-    Get last 1000 comments posted by the targeted user (the 1000 limit is hard coded...)
+    Get data from the user's reddit account.
+    we can only access the last 1000 comments posted by the targeted user (the limit is hard coded...)
     :param reddit: an instance of praw's reddit
     :param username: string, the reddit-username of the target
     :return: comment_list, a list of strings, the comments of the target
--- get_tweets_from_specified_account.py
+++ get_tweets_from_specified_account.py
@@ -3,59 +3,62 @@
 
     Import Twitter data from a specified accound
 """
-## import dependencies
 from tweepy import OAuthHandler
 from tweepy import API
 from tweepy import Cursor
 from datetime import datetime, date, time, timedelta
 
-## details for twitter application
-consumer_key=""
-consumer_secret=""
-access_token=""
-access_token_secret=""
+def identification_twitter():
+    """
+    create instance for the data collection API
+    :return: an instace that can be used to retrieve information on the user
+    """
+    ## details for twitter application
+    consumer_key=""
+    consumer_secret=""
+    access_token=""
+    access_token_secret=""
+    ## setup for tweetpy
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    auth_api = API(auth)
+    return auth_api
 
-## setup for tweetpy
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-auth_api = API(auth)
+def get_twitter_data(twitter, username):
+    """
+    Get data from the user's twitter account.
+    :param twitter: an instance of tweepy'API
+    :param username: string, the twitter-username of the target
+    :return: tweet_count. int, the number of tweet colected
+             status_list, a list of strings, the statuses posted by the user over the last month
+    """
+    ## optain Twiiter_User_Object (TUO)
+    TUO = twitter.get_user(user_name)
+    ## example data ...
+##    name = TUO.name
+##    screen_name = TUO.screen_name
+##    description = TUO.description
+##    total_number_of_tweets = str(TUO.statuses_count)
+##    number_of_followed = str(TUO.friends_count)
+##    number_of_followers = str(TUO.followers_count)
+##    creation_date = item.created_at
 
-## get user_name from the website
-user_name = ""
-
-## optain Twiiter_User_Object (TUO)
-
-TUO = auth_api.get_user(user_name)
-
-## example data
-
-name = TUO.name
-screen_name = TUO.screen_name
-description = TUO.description
-total_number_of_tweets = str(TUO.statuses_count)
-number_of_followed = str(TUO.friends_count)
-number_of_followers = str(TUO.followers_count)
-creation_date = item.created_at
-
-## get user's timeline over the last month
-## produce a dictionary entry ?
-
-tweet_count = 0
-end_date = datetime.utcnow() - timedelta(month=1)
-for status in Cursor(auth_api.user_timeline, id=target).items():
-    ## exit loop after specified date.
-    if status.created_at < end_date:
-        break
-    tweet_count += 1
-    if hasattr(status, "entities"):
-        ## the attribute entities contains hashtags, urls, symbols, media and user_mentions
-    if hasattr(status, "text"):
-        ## text content of the tweet
-    if hasattr(status, "text"):
-        ## text content of the tweet
-    if hasattr(status, "favorite_count"):
-        ## number of times this status was favorited
-    if hasattr(status, "retweet_count"):
-        ## number of times this status was retweeted
-    if hasattr(status, "in_reply_to_user_id_str"):
-        ## user_id of the person being responded too
+    ## get user's timeline over the last month
+    tweet_count = 0
+    status_list = []
+    end_date = datetime.utcnow() - timedelta(month=1)
+    for status in Cursor(auth_api.user_timeline, id=target).items():
+        ## exit loop after specified date.
+        if status.created_at < end_date:
+            break
+        tweet_count += 1
+        if hasattr(status, "entities"):
+            ## the attribute entities contains hashtags, urls, symbols, media and user_mentions
+        if hasattr(status, "text"):
+            ## text content of the tweet
+            status_list.append(status.text)
+        if hasattr(status, "favorite_count"):
+            ## number of times this status was favorited
+        if hasattr(status, "retweet_count"):
+            ## number of times this status was retweeted
+    return tweet_count, status_list
